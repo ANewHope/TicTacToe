3eeab153362d62dd265e1c1216482171c4312787
Atli Freyr Einarsson <asfalt@i-c39488ed.greenqloud.internal>
2014-10-30 04:30:02 +0000

    Added Base Classes and most variables, and some simple methods

---
+++ src/main/java/Board.java
@@ -0,0 +1,55 @@
+public class Board() {
+  Cell[][] cell;
+  public Board() {
+    int rows = GameMain.ROWS;
+    int cols = GameMain.COLS;
+    cell = new Cell[rows][cols];
+    for(int r = 0; r < rows; r++) {
+      for(int c = 0; c < cols; c++) {
+        cell[r][c] = new Cell(r,c);
+      }
+    }
+  }
+  public boolean checkTie() {
+    if(checkWin()) return false;
+    for(int r = 0; r < GameMain.ROWS; r++) {
+      for(int c = 0; c < GameMain.COLS; c++) {
+        if(cell[r][c].content == CellContent.EMPTY) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  public boolean checkWin() {
+    return (checkRows() || checkCols() || checkDiags());
+  }
+  public void paint(Graphics G) {
+
+  }
+
+  private boolean checkRows() {
+    for(int i = 0; i < 3; i++) {
+      if((cell[i][0].content == cell[i][1].content) && (cell[i][0].content == cell[i][2].content)) {
+       return true;
+    }
+    return false;
+  }
+  private boolean checkCols() {
+    for(int i = 0; i < 3; i++) {
+      if((cell[0][i].content == cell[1][i].content) && (cell[0][i].content == cell[2][i].content)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  private boolean checkDiags() {
+    if((cell[0][0].content == cell[1][1].content) && (cell[0][0].content == cell[2][2].content)) {
+      return true;
+    }
+    else if((cell[2][0].content == cell[1][1].content) && (cell[2][0].content == cell[0][2].content)) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ src/main/java/Cell.java
@@ -0,0 +1,12 @@
+public class Cell {
+  CellContent content;
+  int row, col;
+  public Cell(int row, int col) {
+    this.row = row;
+    this.col = col;
+    content = CellContent.EMPTY;
+  }
+  public void drawCell(Graphics g) {
+    
+  }
+}
