3cea8f9e9423caa46fc9a34678e32d9b0b13cd1f
Aron Freyr <aronh13@ru.is>
2014-11-02 05:28:54 +0000

    added a reset button

--- src/main/java/GameWeb.java
+++ src/main/java/GameWeb.java
@@ -16,6 +16,16 @@ public class GameWeb implements SparkApplication {
 
   public void init() {
     final Game game = new Game();
+
+    post(new Route ("/init") {
+         @Override
+            public Object handle(Request request, Response response) {
+                response.status(200);
+                game.init();
+                return buildTable(game);
+            }
+         });
+
     post(new Route("/playerMove") {
       @Override
       public Object handle(Request request, Response response) {
@@ -28,35 +38,39 @@ public class GameWeb implements SparkApplication {
              }
              if (game.playerMove(x, y)) {
                     response.status(200);
-
-                    StringBuilder tableBoard = new StringBuilder();
-                    tableBoard.append("<table class=\"table board\" >\n" +
-                            "                <tbody>\n" +
-                            "                <tr>\n" +
-                            "                    <td id=\"00\" class=\"cell\">"+ game.board.cell[0][0].content.toString() +"</td>\n" +
-                            "                    <td id=\"01\"class=\"cell\">"+ game.board.cell[0][1].content.toString() +"</td>\n" +
-                            "                    <td id=\"02\"class=\"cell\">"+ game.board.cell[0][2].content.toString() +"</td>\n" +
-                            "                </tr>\n" +
-                            "                <tr>\n" +
-                            "                    <td id=\"10\"class=\"cell\">"+ game.board.cell[1][0].content.toString() +"</td>\n" +
-                            "                    <td id=\"11\"class=\"cell\">"+ game.board.cell[1][1].content.toString() +"</td>\n" +
-                            "                    <td id=\"12\"class=\"cell\">"+ game.board.cell[1][2].content.toString() +"</td>\n" +
-                            "                </tr>\n" +
-                            "                <tr>\n" +
-                            "                    <td id=\"20\"class=\"cell\">"+ game.board.cell[2][0].content.toString() +"</td>\n" +
-                            "                    <td id=\"21\"class=\"cell\">"+ game.board.cell[2][1].content.toString() +"</td>\n" +
-                            "                    <td id=\"22\"class=\"cell\">"+ game.board.cell[2][2].content.toString() +"</td>\n" +
-                            "                </tr>\n" +
-                            "                </tbody>\n" +
-                            "            </table>");
-
                     game.nextPlayer();
-                    return tableBoard.toString();
+                    return buildTable(game);
                 }
             response.status(500);
             return response;
       }
     });
   }
+  public String buildTable(Game game) {
+
+    StringBuilder tableBoard = new StringBuilder();
+        tableBoard.append("<table class=\"table board\" >\n" +
+            "                <tbody>\n" +
+            "                <tr>\n" +
+            "                    <td id=\"00\" class=\"cell\">"+ game.board.cell[0][0].content.toString() +"</td>\n" +
+            "                    <td id=\"01\"class=\"cell\">"+ game.board.cell[0][1].content.toString() +"</td>\n" +
+            "                    <td id=\"02\"class=\"cell\">"+ game.board.cell[0][2].content.toString() +"</td>\n" +
+            "                </tr>\n" +
+            "                <tr>\n" +
+            "                    <td id=\"10\"class=\"cell\">"+ game.board.cell[1][0].content.toString() +"</td>\n" +
+            "                    <td id=\"11\"class=\"cell\">"+ game.board.cell[1][1].content.toString() +"</td>\n" +
+            "                    <td id=\"12\"class=\"cell\">"+ game.board.cell[1][2].content.toString() +"</td>\n" +
+            "                </tr>\n" +
+            "                <tr>\n" +
+            "                    <td id=\"20\"class=\"cell\">"+ game.board.cell[2][0].content.toString() +"</td>\n" +
+            "                    <td id=\"21\"class=\"cell\">"+ game.board.cell[2][1].content.toString() +"</td>\n" +
+            "                    <td id=\"22\"class=\"cell\">"+ game.board.cell[2][2].content.toString() +"</td>\n" +
+            "                </tr>\n" +
+            "                </tbody>\n" +
+            "            </table>");
+        return tableBoard.toString();
+
+  }
 }
+
 
