f7b1031e3b6b143a3fa0241bc18eb6202e569761
Atli Freyr Einarsson <asfalt@i-c39488ed.greenqloud.internal>
2014-11-02 13:12:56 +0000

    finished commenting Board Class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -16,6 +16,9 @@ public class Board {
     }
   }
   
+  /**
+  * This method initializes a 3x3 board, with all cells empty.
+  */
   public void init() {
     winner = Content.EMPTY;
     for(int r = 0; r < ROWS; r++) {
@@ -25,6 +28,12 @@ public class Board {
     }
   }
 
+  /**
+  * This method checks if there is a tie, which only occurs
+  * when there are no empty cells and neither player has won.
+  *
+  * @return    true if there is a tie, false otherwise.
+  */
   public boolean checkTie() {
     if(checkWin()) return false;
     for(int r = 0; r < ROWS; r++) {
@@ -37,6 +46,12 @@ public class Board {
     return true;
   }
 
+  /**
+  * This method checks if either player has won the game.
+  *
+  * @return    true if either player has won the game by 
+  *            placing 3 marks in a straight line
+  */
   public boolean checkWin() {
     Content x = Content.EX;
     Content o = Content.ZERO;
@@ -67,6 +82,13 @@ public class Board {
     return false;
   }
 
+  /**
+  * This method checks if a specific player type ('x' or 'o')
+  * has won by placing 3 in a column.
+  *
+  * @param  c  the player type (x/o)
+  * @return    true if player has 3 in a column, false otherwise
+  */
   public boolean checkCols(Content c) {
     for(int i = 0; i < COLS; i++) {
       if((cell[0][i].content == cell[1][i].content) && (cell[0][i].content == cell[2][i].content) && cell[0][i].content == c) {
@@ -76,6 +98,13 @@ public class Board {
     return false;
   }
 
+  /**
+  * This method checks if a specific player type ('x' or 'o')
+  * has won by placing 3 on a diagonal line.
+  *
+  * @param  c  the player type (x/o)
+  * @return    true if player has 3 on a diagonal, false otherwise
+  */
   public boolean checkDiags(Content c) {
     if((cell[0][0].content == cell[1][1].content) && (cell[0][0].content == cell[2][2].content) && cell[0][0].content == c) {
       return true;
